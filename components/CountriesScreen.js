09fb1f8ca1bdd1c94c997a310fa6eda837554c89
shamiulalamupom <1hourprograms@gmail.com>
2025-04-02 16:43:12 +0200

    feat: screens added home and countries.

--- components/CountriesScreen.js
+++ components/CountriesScreen.js
@@ -1,9 +1,74 @@
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+  Text,
+  Button,
+} from "react-native";
+import countries from "../data/countries";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
-export const CountriesScreen = () => {
+const CountriesScreen = () => {
+  const [selectedCountry, setSelectedCountry] = useState(null);
+
+  const handleBack = () => {
+    setSelectedCountry(null);
+  };
+
+  if (selectedCountry) {
+    return (
+      <View>
+        <TouchableOpacity onPress={handleBack}>
+          <Icon name="keyboard-backspace" size={20} />
+        </TouchableOpacity>
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <Image
+            source={{ uri: selectedCountry.flag }}
+            style={{ width: 300, height: 200, resizeMode: "contain" }}
+          />
+          <Text style={{ fontSize: 20, marginTop: 10, fontWeight: "bold" }}>
+            {selectedCountry.name}
+          </Text>
+          <Text style={{ fontSize: 15, marginTop: 10 }}>
+            {`Population: ${selectedCountry.name}`}
+          </Text>
+          <Text style={{ fontSize: 10, marginTop: 10 }}>
+            {selectedCountry.description}
+          </Text>
+        </View>
+      </View>
+    );
+  }
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Countries Screen ABC</Text>
+    <View style={{ flex: 1, padding: 10 }}>
+      <ScrollView>
+        {countries.map((country, index) => (
+          <View
+            key={index}
+            style={{
+              marginBottom: 5,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <TouchableOpacity onPress={() => setSelectedCountry(country)}>
+              <Image
+                source={{ uri: country.flag }}
+                style={{
+                  width: 500,
+                  height: 400,
+                  resizeMode: "contain",
+                }}
+              />
+            </TouchableOpacity>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 };
--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,9 +1,13 @@
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
 
 const HomeScreen = ({ navigation }) => {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen ABC</Text>
+      <Text>Welcome to the Home Screen</Text>
+      <Image
+        source={{ uri: "https://placehold.co/600x400" }}
+        style={{ width: 150, height: 150, marginTop: 20 }}
+      />
     </View>
   );
 };
